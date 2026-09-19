baa74e0583c80f7eef2c6ec92e867704e04da371
Harrison M. Ndugba BCS <ndu.harry02@gmail.com>
2026-09-19 23:31:49 +0100

    main: ui: removed ugly footer

--- src/components/site-footer.jsx
+++ src/components/site-footer.jsx
@@ -13,24 +13,8 @@ export function SiteFooter() {
   return (
     <footer className="mt-auto border-t border-black/10 bg-white">
       <div className="mx-auto flex max-w-6xl flex-col gap-3 px-4 py-8 text-xs leading-relaxed text-black/55 sm:px-6">
-        <p className="text-black/70">{describeOperator()}</p>
-        <address className="not-italic">
-          {legalIdentity.businessAddress}
-          {needsSeparateAddressForService() ? (
-            <>
-              <br />
-              Address for service: {legalIdentity.addressForService}
-            </>
-          ) : null}
-          <br />
-          <a
-            href={`mailto:${legalIdentity.contactEmail}`}
-            className="font-medium text-black/70 hover:underline"
-          >
-            {legalIdentity.contactEmail}
-          </a>
-        </address>
         <div className="flex gap-4">
+          <Link href="/" className="font-medium hover:underline"></Link>
           <Link href="/terms" className="font-medium hover:underline">
             Terms
           </Link>
